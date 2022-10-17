8527c7ef87e37e8cbbc34d729a64505736461c84
Max Santtos <maxsanttos@hotmail.com>
2022-10-17 12:22:55 -0300

    novas atualizacao java chess project

--- Chess in java/src/application/Program.java
+++ Chess in java/src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        
+        Position position = new Position(3, 5);
+
+        System.out.println(position);
     }
 }
