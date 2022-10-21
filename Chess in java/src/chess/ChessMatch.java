6cbc5601733fe1329fc90bbd4b07aa97e8d95190
Max Santtos <maxsanttos@hotmail.com>
2022-10-21 15:17:53 -0300

    corrigindo alguns erros, e implementando uma nova peca rook black

--- Chess in java/src/chess/ChessMatch.java
+++ Chess in java/src/chess/ChessMatch.java
@@ -28,6 +28,7 @@ public class ChessMatch {
 
     private void initialSetup(){
         placeNewPiece('b',6,new Rook(board,Color.WHITE));
+        placeNewPiece('g',6,new Rook(board,Color.BLACK));
         placeNewPiece('e',8,new King(board,Color.BLACK));
         placeNewPiece('e',1,new King(board,Color.WHITE));
     }
