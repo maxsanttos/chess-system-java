4dff3081117f4792b5b69d0c0e0b0d3f0c60bb75
Max Santtos <maxsanttos@hotmail.com>
2022-10-21 15:10:23 -0300

    novas atualizacoes do projeto chess

--- Chess in java/src/chess/ChessPosition.java
+++ Chess in java/src/chess/ChessPosition.java
@@ -6,10 +6,9 @@ public class ChessPosition {
     private char column;
     private int row;
 
-    public ChessPosition(){}
     public ChessPosition(char column, int row){
-        if (column < 'a' || column > 'h' || row >1 || row > 8) {
-            throw new ChessException("Error instantiating ChessPosition. Valid values are from al to h8.");
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+            throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
         }
         this.column = column;
         this.row = row;
