eebf0c40a77ebd99700d67ab38d57932c1ff437b
Max Santtos <maxsanttos@hotmail.com>
2022-10-22 22:04:27 -0300

    Tratando exceções e limpando a tela

--- Chess in java/src/application/Programa.java
+++ Chess in java/src/application/Programa.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -12,16 +14,26 @@ public class Programa {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         while(true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source:");
-            ChessPosition source = UI.redChessPosition(sc);
-
-            System.out.println();
-            System.out.print("Target:");
-            ChessPosition target = UI.redChessPosition(sc);
-
-            ChessPiece capturChessPiece = chessMatch.performChessMove(source, target);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source:");
+                ChessPosition source = UI.redChessPosition(sc);
+    
+                System.out.println();
+                System.out.print("Target:");
+                ChessPosition target = UI.redChessPosition(sc);
+    
+                ChessPiece capturChessPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+           
 
         }
     }
--- Chess in java/src/application/UI.java
+++ Chess in java/src/application/UI.java
@@ -49,7 +49,7 @@ public class UI {
     
     public static void printBoard(ChessPiece[][] pieces){
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
+            System.out.print((8 - i) + "  ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j]);
             }
@@ -68,7 +68,6 @@ public class UI {
             else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
-            
         }
         System.out.print(" ");
     }
