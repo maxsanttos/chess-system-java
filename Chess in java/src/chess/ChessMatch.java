9936fbaa921ab9efdbd0639138d57d05143aa57a
Max Santtos <maxsanttos@hotmail.com>
2022-11-03 16:07:34 -0300

    Lógica de xeque parte 1

--- Chess in java/src/chess/ChessMatch.java
+++ Chess in java/src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import boardgame.Board;
 import boardgame.Piece;
@@ -14,6 +15,7 @@ public class ChessMatch {
     private int turn;
     private Color currentPlayer;
     private Board board;
+    private boolean check;
 
     private List<Piece> pieceOnTheBoard = new ArrayList<>();
     private List<Piece> capturePieces = new ArrayList<>();
@@ -71,6 +73,17 @@ public class ChessMatch {
         return capturPiece;
     }
     
+    private void undoMove(Position source, Position target,Piece capturPiece){
+        Piece p = board.removePiece(target);
+        board.placePiece(p, source);
+
+        if (capturPiece != null) {
+            board.placePiece(capturPiece, target);
+            capturePieces.remove(capturPiece);
+            pieceOnTheBoard.add(capturPiece);
+        }
+    }
+
     private void validateSourcePosition(Position position){
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
@@ -99,6 +112,21 @@ public class ChessMatch {
     private void placeNewPiece(char column,int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column,row).toPosition());
         pieceOnTheBoard.add(piece);
+    }
+
+
+    private Color opponent(Color color){
+        return (color == Color.BLACK) ? Color.BLACK : Color.WHITE;
+    }
+
+    private ChessPiece King(Color color){
+        List<Piece> list = pieceOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+        for (Piece p : list) {
+            if (p instanceof King) {
+                return (ChessPiece)p;
+            }
+        }
+        throw new IllegalStateException("There is no " + color + "King on the board");
     }
 
     private void initialSetup(){
