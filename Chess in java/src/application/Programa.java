b7e56d0f7e1f87ed186d1214926883e2b7ccfa8b
Max Santtos <maxsanttos@hotmail.com>
2022-11-06 10:39:08 -0300

    Special move - Promotion among others

--- Chess in java/src/application/Programa.java
+++ Chess in java/src/application/Programa.java
@@ -38,6 +38,12 @@ public class Programa {
                 if (capturePiece != null) {
                     captured.add(capturePiece);
                 }
+                
+                if (chessMatch.getPromoted() != null) {
+                    System.out.println("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
             } 
             catch (ChessException e) {
                 System.out.println(e.getMessage());
